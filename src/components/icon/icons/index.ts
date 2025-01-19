5f566f1b59720ae9f67f8be1967511f6391ab9f6
Uğur Kellecioğlu <ugur.kellecioglu@outlook.com>
2025-01-19 15:44:27 +0300

    feat: add new fish icons (Angelfish, Goldfish, Guppy, Oscar, SeaMoss, SeaMossSecond, SeaMossThird)

--- src/components/icon/icons/index.ts
+++ src/components/icon/icons/index.ts
@@ -1,16 +1,40 @@
 import { Component } from 'vue'
+import AngelfishIcon from './AngelfishIcon.vue'
 import ClockIcon from './ClockIcon.vue'
 import GaugeIcon from './GaugeIcon.vue'
+import GoldfishIcon from './GoldfishIcon.vue'
+import GuppyIcon from './GuppyIcon.vue'
+import OscarIcon from './OscarIcon.vue'
 import PauseIcon from './PauseIcon.vue'
 import PlayIcon from './PlayIcon.vue'
+import SeaMoss from './SeaMoss.vue'
+import SeaMossSecond from './SeaMossSecond.vue'
+import SeaMossThird from './SeaMossThird.vue'
 
-export type IconNames = 'Clock' | 'Gauge' | 'Pause' | 'Play'
-
+export type IconNames =
+  | 'Clock'
+  | 'Gauge'
+  | 'Pause'
+  | 'Play'
+  | 'SeaMoss'
+  | 'SeaMossSecond'
+  | 'SeaMossThird'
+  | 'Angelfish'
+  | 'Goldfish'
+  | 'Guppy'
+  | 'Oscar'
 const Icons: Record<IconNames, Component> = {
   Clock: ClockIcon,
   Gauge: GaugeIcon,
   Pause: PauseIcon,
   Play: PlayIcon,
+  SeaMoss: SeaMoss,
+  SeaMossSecond: SeaMossSecond,
+  SeaMossThird: SeaMossThird,
+  Angelfish: AngelfishIcon,
+  Goldfish: GoldfishIcon,
+  Guppy: GuppyIcon,
+  Oscar: OscarIcon,
 }
 
 export default Icons
