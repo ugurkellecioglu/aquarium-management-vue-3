76501e6c35e90385388241e9e30a8a813384b4a9
Uğur Kellecioğlu <ugur.kellecioglu@outlook.com>
2025-01-19 15:48:17 +0300

    feat: integrate PrimeVue and global styles into main application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
+import './assets/global.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import { createApp } from 'vue'
+
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue)
 
 app.mount('#app')
